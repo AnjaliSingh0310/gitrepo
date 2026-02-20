d1368d60bf7e02fecfe23b51d3916341e655130b
Anjali <cavisson.anjali@gmail.com>
2026-02-20 12:56:42 +0530

    wrapper shell

---
+++ run-quality.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Starting Sonar Scan..."
+
+sonar-scanner \
+  -Dsonar.projectKey=AnjaliSingh0310_gitrepo
+  -Dsonar.organization=anjalisingh0310
+  -Dsonar.exclusions=**/TSDB/**
+  -Dsonar.sources=. \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=33f49840787e6388b6ceab0e11da146d09902636
+  
+
+echo "Sonar Scan Completed"
